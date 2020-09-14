93ee30f11abd0ce88b0574aaedad451416d6f9e8
Dani Matuko <matuko305@gmail.com>
2020-09-14 23:51:46 +0300

    hamburger completed

--- js/app.js
+++ js/app.js
@@ -3,9 +3,16 @@ const aboutParagraph = document.querySelector('.about-wrapper p');
 const projectTitle = document.querySelector('#projects-section h2');
 const projects = document.querySelectorAll('.project');
 
+
+const primary_color = '#25b7a0';
+const secondary_color = '#07374a';
+
+
 window.addEventListener('scroll', () => {
     let aboutArea = document.querySelector("#about-section").getBoundingClientRect().top;
     let projectsArea = document.querySelector("#projects-section").getBoundingClientRect().top;
+    let introArea = document.querySelector('#intro').getBoundingClientRect().top;
+
     let screenPosition = window.innerHeight / 1.3;
 
     if (aboutArea < screenPosition) {
@@ -21,4 +28,25 @@ window.addEventListener('scroll', () => {
         });
     }
 
+
+    // const navBar = document.querySelector("#nav-bar");
+    // const midNav = navBar.getBoundingClientRect().bottom / 2;
+
+    // if (introArea < midNav) navBar.style.background = primary_color;
+
+    // if (aboutArea < midNav) navBar.style.background = secondary_color;
+
+    // if (projectsArea < midNav) navBar.style.background = primary_color;
+
 });
+
+
+
+// Close menu on moblie
+if (screen.width <= 550) {
+    const hamburger = document.querySelector("#hamburger");
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('opened');
+    });
+}
