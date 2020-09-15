5abc1ab5b80df60ab14b2eec66ca77752900bcc8
Dani Matuko <matuko305@gmail.com>
2020-09-16 00:36:14 +0300

    mobile nav completed

--- js/app.js
+++ js/app.js
@@ -3,23 +3,21 @@ const aboutParagraph = document.querySelector('.about-wrapper p');
 const projectTitle = document.querySelector('#projects-section h2');
 const projects = document.querySelectorAll('.project');
 
-
 const primary_color = '#25b7a0';
 const secondary_color = '#07374a';
 
-
+/* Scroll Events */
 window.addEventListener('scroll', () => {
     let aboutArea = document.querySelector("#about-section").getBoundingClientRect().top;
     let projectsArea = document.querySelector("#projects-section").getBoundingClientRect().top;
     let introArea = document.querySelector('#intro').getBoundingClientRect().top;
-
     let screenPosition = window.innerHeight / 1.3;
-
+    // About Section
     if (aboutArea < screenPosition) {
         aboutTitle.classList.add('slideUp');
         aboutParagraph.classList.add('slideUp');
     }
-
+    // Projects Section
     if (projectsArea < screenPosition) {
         projectTitle.classList.add('slideUp');
         projects.forEach((project) => {
@@ -42,11 +40,29 @@ window.addEventListener('scroll', () => {
 
 
 
-// Close menu on moblie
+/* Mobile Navigation */
 if (screen.width <= 550) {
     const hamburger = document.querySelector("#hamburger");
+    const menu = document.querySelector("#nav-bar");
+    const menuItem = document.querySelectorAll(" .menu-item ");
+
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('opened');
+        hamburger.classList.contains('opened') ? menu.classList.add('show-menu') : menu.classList.remove('show-menu');
     });
+
+    menuItem.forEach((item) => {
+        item.addEventListener('click', () => {
+            setTimeout(() => {
+                hamburger.classList.remove('opened');
+                menu.classList.remove('show-menu');
+            }, 450);
+
+
+        });
+    });
+
+
+
 }
